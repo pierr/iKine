d5626d875c8c8cd9070c707437bd536d56fbc42d
pierr <pierre.besson7@gmail.com>
2011-05-16 01:41:16 +0200

    initialisation des fichiers pour l application rubyonRails et maj du fichier .gitignore (en faite supression de celui de l app

---
+++ kine_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KineApp::Application.config.secret_token = '2da3babac387a6eee1d1b20c435d869f6e27e83a5dd7db69380dea0a5d15cfe3cea7c29f39967b10a5c9388d4b32f33862e927d2d9c2458afdd8c0446004c287'
