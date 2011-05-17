123ce08693fd52e82bfe21ec555c95007786bc24
pierr <pierre.besson7@gmail.com>
2011-05-17 23:01:42 +0200

    maj utilisation d un helper

--- kine_app/app/helpers/application_helper.rb
+++ kine_app/app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  # Return a title on a per-page basis.
+   def title
+     base_title = "Kine App"
+     if @mode.nil?
+       base_title
+     else
+       "#{base_title} | #{@mode}"
+     end
+   end
 end
