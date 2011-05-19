89d8eda72639a58c3eef21ba571b825772f4ae26
pierr <pierre.besson7@gmail.com>
2011-05-20 01:39:40 +0200

    Mise à jour possible des users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+   before_filter :authenticate, :only => [:edit, :update] #Pour recqérir l'autentification d'un utilisateur
   def new
     @user = User.new
     @title ="Sign up"
@@ -19,4 +20,26 @@ class UsersController < ApplicationController
         render 'new'
       end
   end
+  
+  def edit
+      @user = User.find(params[:id])
+      @title = "Edit user"
+  end
+  
+  def update
+      @user = User.find(params[:id])
+      if @user.update_attributes(params[:user])
+        flash[:success] = "Profile mis a jour."
+        redirect_to @user
+      else
+        @title = "Edit user"
+        render 'edit'
+      end
+    end
+    
+    private
+    
+    def authenticate
+          deny_access unless signed_in?
+    end
 end
