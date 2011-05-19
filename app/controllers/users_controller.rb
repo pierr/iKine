5179e381de2d97d7e488a571aeb2b1b7891386ff
pierr <pierre.besson7@gmail.com>
2011-05-19 16:54:19 +0200

    Le processus de creation de compte est termine

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,21 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
     @mode ="Sign up"
   end
   def show
      @user = User.find(params[:id])
      @mode = @user.surname
    end
-
+   #Permet de creer un nouvel utilisateur.
+   def create 
+     @user = User.new(params[:user])
+     if @user.save
+       flash[:success] = "Bienvenue sur iKine ".concat(@user.firstname).concat(" !")
+       redirect_to user_path(@user)
+      else
+        @mode = "Sign Up"
+        render 'new'
+      end
+  end
 end
