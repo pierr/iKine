81f2a8ee2316aec6c105294afa27a46f729ebc29
pierr <pierre.besson7@gmail.com>
2011-05-23 20:29:07 +0200

    nettoyage du code js et css

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,21 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+/**
+*Fonction pour changer les onglets
+* @see http://www.johnstyle.fr/langage/javascript/creer-des-onglets-facilement/
+*/
+function changeOnglet(_this){
+	var getOnglets 	= document.getElementById('mes_onglets').getElementsByTagName('li');
+	for(var i = 0; i < getOnglets.length; i++){
+		if(getOnglets[i].id){
+			if(getOnglets[i].id == _this.id){
+				getOnglets[i].className = 'mon_onglet_selected';
+				document.getElementById('c' + _this.id).style.display 			= 'block';
+			}
+			else{
+				getOnglets[i].className = 'mon_onglet';
+				document.getElementById('c' + getOnglets[i].id).style.display 	= 'none';
+			}
+		}
+	}			
+}
