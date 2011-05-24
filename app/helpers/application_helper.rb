df1e9606d6e36b3229c1c5336c0390502945ded0
pierr <pierre.besson7@gmail.com>
2011-05-24 17:52:48 +0200

    Ajout d'une meilleur documentation pur les methodes utiles pour gérér les onglets

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -22,8 +22,14 @@ module ApplicationHelper
       link_to title, params.merge(:sort =>column ,:direction => direction , :page=>nil), {:class => css_class}
    end
    
+   ###############################
+   ## METHODES POUR LES ONGLETS ##
+   ###############################
+   
    #Methode qui permet de generer des onglets portant le nom de ceux presents dans le tableau qu'on passe en parametre
    #par defaut on prend le premier
+   #  -onglets est un tableau de string qui contient le nom de chaque onglets
+   #  -selected est le numero de l'onglet qu'on veut sélectionner
    def onglets(onglets, selected = 1)
      html_a_rendre = "
        <div id='mes_onglets'>
@@ -44,12 +50,19 @@ module ApplicationHelper
     return html_a_rendre
    end
    
+   #Methode a appeler avant de commencer a faire les contenus des onglets
    def debut_contenu_onglets
     return 	"<div id='mes_contenus'> <!-- Debut des onglets-->".html_safe
    end
+   
+   #Methode a appeler une fois qu'on a fini d'éditer les contenus des onglets
    def fin_contenu_onglets
     return "</div> <!-- fin des onglet-->".html_safe
    end
+   
+   #methode qui permet de commencer le contenu d'un onglet 
+   #  -num_onglet est le numero de l'onglet qu'on veut définir 
+   #  -is_selected est un boolen qui dit signifie que l'onglet est actif ou non (par defaut il est inactif)
    def debut_contenu_onglet(num_onglet, is_selected = false)
     html_a_rendre = "<div id='co_#{num_onglet}' class='mon_contenu'".html_safe
     if !is_selected
@@ -59,7 +72,12 @@ module ApplicationHelper
     return html_a_rendre
    end
    
+   #Methode à appeler à la fin du remplissage du contenu d'un onglet 
    def fin_contenu_onglet(num_onglet)
      return "</div> <!-- fin de l onglet #{num_onglet}-->".html_safe
    end
+   
+   #######################################
+   ## FIN DESMETHODES POUR LES ONGLETS ##
+   ######################################
 end
