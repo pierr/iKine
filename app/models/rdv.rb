dea9beaf9765aa0f48a4fa7c87ef61069aa4f2c3
pierr <pierre.besson7@gmail.com>
2011-06-21 10:45:39 +0200

    maj annotation

--- app/models/rdv.rb
+++ app/models/rdv.rb
@@ -1,3 +1,22 @@
+# == Schema Information
+# Schema version: 20110616110935
+#
+# Table name: rdvs
+#
+#  id              :integer         not null, primary key
+#  date            :datetime
+#  duree           :integer
+#  a_domicile      :boolean
+#  nouveau_patient :boolean
+#  telephone       :string(255)
+#  email           :string(255)
+#  commentaire     :string(255)
+#  patient_id      :integer
+#  adresse_id      :integer
+#  created_at      :datetime
+#  updated_at      :datetime
+#
+
 class Rdv < ActiveRecord::Base
      belongs_to :patient
   belongs_to :adresse
