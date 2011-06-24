fccfd00eb90e81303cbbd1da18046e65906e0865
Audric Debladis <audric.debladis@gmail.com>
2011-06-24 12:02:04 +0200

    merging problem

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 class PatientsController < ApplicationController
   helper_method :sort_column, :sort_direction
   
@@ -27,6 +28,9 @@ class PatientsController < ApplicationController
     @civilite = @patient.civilite
     
     @ordonnances = @patient.ordonnances
+    @onglets = ["Coordonn�es","Dossier m�dical", "Carte"]
+    @onglets_selected = 1
+    
     
   end
   
@@ -35,6 +39,11 @@ class PatientsController < ApplicationController
     @patient = Patient.new
     @adresse = Adresse.new
     @ville = Ville.new
+      if (!params[:rdv].nil?)
+        @retour_rdv=true
+      else
+        @retour_rdv=false
+      end
     
     @civiliteList = Civilite.all
   end
@@ -46,16 +55,45 @@ class PatientsController < ApplicationController
     
       ville_id = params[:ville][:ville_token]
     @adresse.code_insee_id= Ville.find(ville_id).code_insee.id
+   
     
     if(@patient.valid? && @adresse.valid?)
        @adresse.save
        @patient.adresse=@adresse # pour setter correctement l'adresse id de patient
        @patient.save
-       redirect_to(@patient) # aucune erreur => on affiche la page en mode view
-    else # sinon, alors on a des erreurs et on réaffiche la page en mode new
+       puts "_____________________RDV______________"
+       puts params[:rdv]
+       if !params[:rdv].nil?
+           redirect_to(:controller=>"rdvs", :action => "new", :patient_id => @patient)
+        else
+          redirect_to(@patient) # aucune erreur => on affiche la page en mode view
+        end
+    else # sinon, alors on a des erreurs et on r�affiche la page en mode new
        render 'new'
     end
   end
+  
+  #m�me fonction que create mais avec un retour vers un nouveau rendez-vous
+  def retour_to_rdv
+     @title="mode create"
+     @patient = Patient.new(params[:patient])
+     @adresse = Adresse.new(params[:adresse])
+
+       ville_id = params[:ville][:ville_token]
+     @adresse.code_insee_id= Ville.find(ville_id).code_insee.id
+
+
+     if(@patient.valid? && @adresse.valid?)
+        @adresse.save
+        @patient.adresse=@adresse # pour setter correctement l'adresse id de patient
+        @patient.save
+        puts "_________________ICI_________________"
+        redirect_to(:controller=>"rdvs", :action => "new", :patient_id => @patient) # aucune erreur => on affiche la page en mode view
+     else # sinon, alors on a des erreurs et on r�affiche la page en mode new
+        render 'new'
+     end
+   end
+  
 
   def edit
     @title="mode edit"
@@ -68,18 +106,25 @@ class PatientsController < ApplicationController
   end
   
   def update
+    
+    
+    
+    
+    
     @title="mode update"
     @patient = Patient.find(params[:id])
     @adresse = @patient.adresse
     @patient.attributes=(params[:patient])
     @adresse.attributes=(params[:adresse])
     
+    
     ville_id = params[:ville][:ville_token]
     @adresse.code_insee_id= Ville.find(ville_id).code_insee.id
     
+    
     if(@patient.valid? && @adresse.valid?)
        @adresse.save
-       @patient.adresse=@adresse # pour setter correctement l'adresse id de patient mais je pense que c'est pas utile en mode edit... ça dépendra de si l'adresse était existante blablabla
+       @patient.adresse=@adresse # pour setter correctement l'adresse id de patient mais je pense que c'est pas utile en mode edit... �a d�pendra de si l'adresse �tait existante blablabla
        @patient.save
        redirect_to(@patient) # aucune erreur => on affiche la page en mode view
     else
@@ -105,5 +150,12 @@ class PatientsController < ApplicationController
   #trouve la direction de tri
   def sort_direction
      %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
+  end
+  
+  
+    def map
+     @patient = Patient.find(params[:id])
+     @adresse = @patient.adresse
+     render 'map'
   end
 end
